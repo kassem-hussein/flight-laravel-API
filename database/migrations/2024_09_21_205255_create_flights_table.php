67cd92e2cee1c2832f5e2bcfdd5f7709443eee7c
KASSEM HUSSEIN <kassem.learn@gmail.com>
2024-09-22 00:03:13 +0300

    Add migrations

---
+++ database/migrations/2024_09_21_205255_create_flights_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->string('flight_number');
+            $table->string('departure_airport');
+            $table->string('arrival_airport');
+            $table->date('departure_date');
+            $table->time("departure_time");
+            $table->date('arrival_date');
+            $table->time("arrival_time");
+            $table->decimal('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flights');
+    }
+};
